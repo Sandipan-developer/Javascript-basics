c366afc8a084e6c86827d2de27d37fe5572147fa
Sandipan-developer <dassandipan5400@gmail.com>
2025-11-10 02:41:03 +0000

    datatypes learned

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";//treat all code as newer version of js
+
+//alert(3+3) //we are using node js ,not browser
+
+let a="sandipan"
+
+let age=19
+let isLoddedIn=false
+let state
+/*number
+  boolean=true/false
+  string
+  bigint=> >2^53
+  undefined
+  symbol=>uniqueness
+  null=>stand alonevalue(type->object) */
+console.log(typeof state)

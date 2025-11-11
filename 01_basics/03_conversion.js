9267d90887515598c17bb5e8188c00f2c90de7f6
Sandipan-developer <dassandipan5400@gmail.com>
2025-11-11 02:23:35 +0000

    changing3

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,5 +1,11 @@
 let age;
-console.log(typeof age)
+// console.log(typeof age)
 
 let numAge=Number(age)
-console.log(typeof numAge,numAge)
+// console.log(typeof numAge,numAge)
+
+// console.log(1+5+6+"7")
+// console.log("8"+5+7)
+
+console.log(true)
+console.log(+false)

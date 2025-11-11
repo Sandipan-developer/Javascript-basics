8b0b4afceb48bdc1b4288f540123247581872ed7
Sandipan-developer <dassandipan5400@gmail.com>
2025-11-11 02:53:46 +0000

    after commiting 04

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log("4">1)
+// console.log(5>"1")
+
+// console.log(null>0)
+// console.log(null>=0)
+// console.log(null==0)
+
+// >,>=,<,<= (comparisons)act differently than ==(equality check)
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+
+// ===(strict equality check,checks datatype also)
